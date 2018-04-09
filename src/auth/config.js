c86940e13d32d79e5f2d04cde5eced6dced64ce1
jayeshpp <jayeshpp007@gmail.com>
2018-04-09 18:25:18 +0530

    adding product listing page

---
+++ src/auth/config.js
@@ -0,0 +1,7 @@
+const Constants = {
+    endpoint: 'http://localhost:81/wordpress/wordpress',
+    consumerKey : 'ck_f90456c85acc975e114345ebd0c845a3bd5f0639',
+    consumerSecret : 'cs_85eea533be51d8428b6fd5a08680d4b0b5226c82'
+}
+
+module.exports = Constants
